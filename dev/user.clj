6015c32be43ee5747f8ea66bd36001bfd9e2bceb
John Kane <john.kane@spartan-solutions.com>
2014-06-11 23:19:46 +0100

    Moved the munging code into the core namespace.

--- dev/user.clj
+++ dev/user.clj
@@ -12,32 +12,10 @@
    [clojure.tools.namespace.repl :refer (refresh refresh-all)]
    [incanter.io :as io]
    [incanter.core :as incant]
-   [landregistry]))
+   [landregistry :as lr]))
 
-(def columns [:id :price :date-of-transfer :postcode :property-type :old-or-new :duration :paon :saon :street :locality :town :district :county :record-status :occurrence])
-
-(defn add-town-occurence-column [dataset]
-  (incant/conj-cols dataset (take (incant/nrow dataset) (repeat 1))))
-
-(defonce sales
-  (-> (io/read-dataset "./data/pp-monthly.csv")
-      add-town-occurence-column
-      (incant/col-names columns)))
-
-(defn show-towns [dataset]
-  (incant/with-data dataset
-    (incant/view (incant/$ [:town]))))
-
-(defn top-rows [n dataset]
-  (incant/sel dataset :rows (range n)))
-
-(defn town-freq [dataset]
-  (->> dataset
-       (incant/$rollup :count :occurrence [:town])
-       (incant/$order :occurrence :desc)
-       (top-rows 20)
-       incant/view))
+(defonce sales (io/read-dataset "./data/pp-monthly.csv"))
 
 (comment
-  (show-towns sales)
-  (town-freq sales))
+  (lr/show-towns sales)
+  (lr/town-freq sales))
--- src/landregistry.clj
+++ src/landregistry.clj
@@ -1,3 +1,27 @@
-(ns landregistry)
+(ns landregistry
+  (:require [incanter.core :as incant]))
 
+(def columns [:id :price :date-of-transfer :postcode :property-type :old-or-new :duration :paon :saon :street :locality :town :district :county :record-status :occurrence])
 
+(defn add-town-occurence-column [dataset]
+  (incant/conj-cols dataset (take (incant/nrow dataset) (repeat 1))))
+
+(defn prepare-dataset [dataset]
+  (-> dataset
+      add-town-occurence-column
+      (incant/col-names columns)))
+
+(defn top-rows [n dataset]
+  (incant/sel dataset :rows (range n)))
+
+(defn town-freq [dataset]
+  (->> dataset
+       prepare-dataset
+       (incant/$rollup :count :occurrence [:town])
+       (incant/$order :occurrence :desc)
+       (top-rows 20)
+       incant/view))
+
+(defn show-towns [dataset]
+  (incant/with-data dataset
+    (incant/view (incant/$ [:town]))))
