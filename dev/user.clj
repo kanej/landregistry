78a670c3d8bf8d845bfa0343204ba332838fe967
John Kane <john.kane@spartan-solutions.com>
2014-06-11 23:09:55 +0100

    Added function to show top 20 occurring towns.

--- dev/user.clj
+++ dev/user.clj
@@ -14,10 +14,30 @@
    [incanter.core :as incant]
    [landregistry]))
 
-(defonce sales (io/read-dataset "./data/pp-monthly.csv"))
+(def columns [:id :price :date-of-transfer :postcode :property-type :old-or-new :duration :paon :saon :street :locality :town :district :county :record-status :occurrence])
+
+(defn add-town-occurence-column [dataset]
+  (incant/conj-cols dataset (take (incant/nrow dataset) (repeat 1))))
+
+(defonce sales
+  (-> (io/read-dataset "./data/pp-monthly.csv")
+      add-town-occurence-column
+      (incant/col-names columns)))
 
 (defn show-towns [dataset]
   (incant/with-data dataset
-    (incant/view (incant/$ [:col12]))))
+    (incant/view (incant/$ [:town]))))
 
-#_(show-towns sales)
+(defn top-rows [n dataset]
+  (incant/sel dataset :rows (range n)))
+
+(defn town-freq [dataset]
+  (->> dataset
+       (incant/$rollup :count :occurrence [:town])
+       (incant/$order :occurrence :desc)
+       (top-rows 20)
+       incant/view))
+
+(comment
+  (show-towns sales)
+  (town-freq sales))
