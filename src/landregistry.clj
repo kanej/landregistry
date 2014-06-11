15c422b87cf109210f9d7d2d646301671ecdc664
John Kane <john.kane@spartan-solutions.com>
2014-06-11 23:42:36 +0100

    Clean up and doc strings.

--- src/landregistry.clj
+++ src/landregistry.clj
@@ -1,20 +1,32 @@
 (ns landregistry
   (:require [incanter.core :as incant]))
 
-(def columns [:id :price :date-of-transfer :postcode :property-type :old-or-new :duration :paon :saon :street :locality :town :district :county :record-status :occurrence])
+(def columns [:id :price :date-of-transfer :postcode :property-type :old-or-new :duration :paon :saon :street :locality :town :district :county :record-status])
 
-(defn add-town-occurence-column [dataset]
+(defn- add-town-occurence-column [dataset]
   (incant/conj-cols dataset (take (incant/nrow dataset) (repeat 1))))
 
-(defn prepare-dataset [dataset]
+(defn- prepare-dataset
+  "Prepare the dataset for analysis.
+
+   This consists of two steps: firstly, add a column of 1s to
+   provide a hook for count aggregations. Secondly add in column names
+   for ease."
+  [dataset]
   (-> dataset
       add-town-occurence-column
-      (incant/col-names columns)))
+      (incant/col-names (conj columns :occurrence))))
 
-(defn top-rows [n dataset]
+(defn- top-rows [n dataset]
   (incant/sel dataset :rows (range n)))
 
-(defn town-freq [dataset]
+(defn top-twenty-towns-by-sales
+  "Given the land registry monthly dataset, return the top 20
+   towns with the most sales.
+
+   Town is defined by the town/city column of the land registry
+   dataset."
+  [dataset]
   (->> dataset
        prepare-dataset
        (incant/$rollup :count :occurrence [:town])
@@ -22,6 +34,8 @@
        (top-rows 20)
        incant/view))
 
-(defn show-towns [dataset]
+(defn show-towns
+  "Show the town column of the land registry month dataset."
+  [dataset]
   (incant/with-data dataset
-    (incant/view (incant/$ [:town]))))
+    (incant/view (incant/$ [:col12]))))
